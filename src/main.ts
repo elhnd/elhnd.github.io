0b6b85f971c97aa3566eac35f4b1a7be6bd8e1e6
EL Hadji NDIAYE <eldji22@hotmail.fr>
2024-01-12 17:48:25 +0100

    add images path in api ressources

--- src/main.ts
+++ src/main.ts
@@ -66,49 +66,49 @@ const handlers = [
       { code: "S",
         description: "Model S",
         colors: [
-          {code: "white", description: "Pearl White Multi-Coat", price: 0},
-          {code: "black", description: "Solid Black", price: 0},
-          {code: "blue", description: "Deep Blue Metallic", price: 0},
-          {code: "grey", description: "Stealth Grey", price: 0},
-          {code: "red", description: "Ultra Red", price: 0}
+          {code: "white", description: "Pearl White Multi-Coat", price: 0, image: "../assets/img/tesla/S/white.jpg"},
+          {code: "black", description: "Solid Black", price: 0, image: "../assets/img/tesla/S/black.jpg"},
+          {code: "blue", description: "Deep Blue Metallic", price: 0, image: "../assets/img/tesla/S/blue.jpg"},
+          {code: "grey", description: "Stealth Grey", price: 0, image: "../assets/img/tesla/S/grey.jpg"},
+          {code: "red", description: "Ultra Red", price: 0, image: "../assets/img/tesla/S/red.jpg"}
         ]
       },
       { code: "X",
         description: "Model X",
         colors: [
-          {code: "white", description: "Pearl White Multi-Coat", price: 0},
-          {code: "black", description: "Solid Black", price: 0},
-          {code: "blue", description: "Deep Blue Metallic", price: 0},
-          {code: "grey", description: "Stealth Grey", price: 0},
-          {code: "red", description: "Ultra Red", price: 0}
+          {code: "white", description: "Pearl White Multi-Coat", price: 0, image: "../assets/img/tesla/X/white.jpg"},
+          {code: "black", description: "Solid Black", price: 0, image: "../assets/img/tesla/X/black.jpg"},
+          {code: "blue", description: "Deep Blue Metallic", price: 0, image: "../assets/img/tesla/X/blue.jpg"},
+          {code: "grey", description: "Stealth Grey", price: 0, image: "../assets/img/tesla/X/grey.jpg"},
+          {code: "red", description: "Ultra Red", price: 0, image: "../assets/img/tesla/X/red.jpg"}
         ]
       },
       { code: "C",
         description: "Cybertruck",
         colors: [
-          {code: "grey", description: "Stainless Steel", price: 0},
-          {code: "black", description: "Satin Black", price: 6500},
-          {code: "white", description: "Satin White", price: 6500}
+          {code: "grey", description: "Stainless Steel", price: 0, image: "../assets/img/tesla/C/grey.jpg"},
+          {code: "black", description: "Satin Black", price: 6500, image: "../assets/img/tesla/C/black.jpg"},
+          {code: "white", description: "Satin White", price: 6500, image: "../assets/img/tesla/C/white.jpg"}
         ]
       },
       { code: "3",
         description: "Model 3",
         colors: [
-          {code: "white", description: "Pearl White Multi-Coat", price: 1000},
-          {code: "black", description: "Solid Black", price: 1500},
-          {code: "blue", description: "Deep Blue Metallic", price: 1000},
-          {code: "grey", description: "Midnight Silver Metallic", price: 0},
-          {code: "red", description: "Red Multi-Coat", price: 2000}
+          {code: "white", description: "Pearl White Multi-Coat", price: 1000, image: "../assets/img/tesla/3/white.jpg"},
+          {code: "black", description: "Solid Black", price: 1500, image: "../assets/img/tesla/3/black.jpg"},
+          {code: "blue", description: "Deep Blue Metallic", price: 1000, image: "../assets/img/tesla/3/blue.jpg"},
+          {code: "grey", description: "Midnight Silver Metallic", price: 0, image: "../assets/img/tesla/3/grey.jpg"},
+          {code: "red", description: "Red Multi-Coat", price: 2000, image: "../assets/img/tesla/3/red.jpg"}
         ]
       },
       { code: "Y",
         description: "Model Y",
         colors: [
-          {code: "white", description: "Pearl White Multi-Coat", price: 1000},
-          {code: "black", description: "Solid Black", price: 2000},
-          {code: "blue", description: "Deep Blue Metallic", price: 1000},
-          {code: "grey", description: "Midnight Silver Metallic", price: 0},
-          {code: "red", description: "Red Multi-Coat", price: 2000}
+          {code: "white", description: "Pearl White Multi-Coat", price: 1000, image: "../assets/img/tesla/Y/white.jpg"},
+          {code: "black", description: "Solid Black", price: 2000, image: "../assets/img/tesla/Y/black.jpg"},
+          {code: "blue", description: "Deep Blue Metallic", price: 1000, image: "../assets/img/tesla/Y/blue.jpg"},
+          {code: "grey", description: "Midnight Silver Metallic", price: 0, image: "../assets/img/tesla/Y/grey.jpg"},
+          {code: "red", description: "Red Multi-Coat", price: 2000, image: "../assets/img/tesla/Y/red.jpg"}
         ]
       }
 
